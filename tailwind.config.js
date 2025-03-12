0fcbbf7b60213051b54b8b92830e1d47e5d4b518
MalikUmair972 <mu501362@gmail.com>
2025-03-12 22:58:01 +0500

    Tailwind Css Install

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",  // Ensure this is set to match Angular's file types
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
