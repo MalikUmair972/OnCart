38a644e261dbf5c202007a4d2f2b0eb58e97b443
MalikUmair972 <mu501362@gmail.com>
2025-03-21 00:08:52 +0500

    Component Module Update

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { SelectDropDownModule } from 'ngx-select-dropdown';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
 import { NavbarComponent } from './navbar/navbar.component';
-
-
+import { SelectDropdownComponent } from './select-dropdown/select-dropdown.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, SelectDropdownComponent],
   imports: [
     CommonModule,
     FormsModule,
     MatInputModule,
     MatSelectModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatIconModule,
+    SelectDropDownModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule
   ],
-  exports: [
-    NavbarComponent
-  ]
+  exports: [NavbarComponent, SelectDropdownComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
