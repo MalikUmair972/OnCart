29c82d4a310262c04b472083befb32d22a5c0d15
MalikUmair972 <mu501362@gmail.com>
2025-03-30 00:02:17 +0500

    Products Arryay add

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss',
+})
+export class ProductComponent {
+  products = [
+    {
+      Image: 'assets/product.png',
+      name: 'New S24 Ultra 5G phone 7.3“ Dual SIM Unlocked Android Mobile Phone 6G+128GB',
+      raing: '4.5',
+      sold: '100k+',
+      price: 'Rs 499,999',
+    },
+    {
+      Image: 'assets/product.png',
+      name: 'New S24 Ultra 5G phone 7.3“ Dual SIM Unlocked Android Mobile Phone 6G+128GB',
+      raing: '4.5',
+      sold: '100k+',
+      price: 'Rs 499,999',
+    },
+    {
+      Image: 'assets/product.png',
+      name: 'New S24 Ultra 5G phone 7.3“ Dual SIM Unlocked Android Mobile Phone 6G+128GB',
+      raing: '4.5',
+      sold: '100k+',
+      price: 'Rs 499,999',
+    },
+    {
+      Image: 'assets/product.png',
+      name: 'New S24 Ultra 5G phone 7.3“ Dual SIM Unlocked Android Mobile Phone 6G+128GB',
+      raing: '4.5',
+      sold: '100k+',
+      price: 'Rs 499,999',
+    },
+    {
+      Image: 'assets/product.png',
+      name: 'New S24 Ultra 5G phone 7.3“ Dual SIM Unlocked Android Mobile Phone 6G+128GB',
+      raing: '4.5',
+      sold: '100k+',
+      price: 'Rs 499,999',
+    },
+  ];
+}
