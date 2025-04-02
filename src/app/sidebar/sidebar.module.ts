050b108a7b0a7cd57d7590f2755358d8c1a321c1
MalikUmair972 <mu501362@gmail.com>
2025-04-03 00:57:54 +0500

    Slider Add

--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 import { PriceSelectorComponent } from './price-selector/price-selector.component';
-
-
+import { SideComponent } from './side/side.component';
 
 @NgModule({
-  declarations: [PriceSelectorComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [PriceSelectorComponent]
+  declarations: [PriceSelectorComponent, SideComponent],
+  imports: [CommonModule, MatSliderModule, FormsModule, MatButtonModule, MatIconModule],
+  exports: [PriceSelectorComponent, SideComponent],
 })
-export class SidebarModule { }
+export class SidebarModule {}
