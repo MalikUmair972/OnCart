8296ebb51ab63043fa7b57081542d251173d0b1d
MalikUmair972 <mu501362@gmail.com>
2025-04-04 00:57:33 +0500

    Check boxes Add

--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -4,13 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import { PriceSelectorComponent } from './price-selector/price-selector.component';
 import { SideComponent } from './side/side.component';
+import { BrandSelectorComponent } from './brand-selector/brand-selector.component';
+import { RatingSelectorComponent } from './rating-selector/rating-selector.component';
 
 @NgModule({
-  declarations: [PriceSelectorComponent, SideComponent],
-  imports: [CommonModule, MatSliderModule, FormsModule, MatButtonModule, MatIconModule],
+  declarations: [PriceSelectorComponent, SideComponent, BrandSelectorComponent, RatingSelectorComponent],
+  imports: [CommonModule, MatSliderModule, FormsModule, MatButtonModule, MatIconModule, MatCheckboxModule],
   exports: [PriceSelectorComponent, SideComponent],
 })
 export class SidebarModule {}
