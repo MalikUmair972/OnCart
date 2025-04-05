3249ae6b566684f903dd426d65f7cff68f5430e1
MalikUmair972 <mu501362@gmail.com>
2025-04-06 00:33:49 +0500

    Category UI done

---
+++ src/app/components/categories/categories.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.scss',
+})
+export class CategoriesComponent {
+  categories = [
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+    {
+      Image: 'assets/gaming.jpg',
+      categoryName: 'Gaming Accessories',
+      categoryLink: 'Explore All Gaming Products',
+    },
+  ];
+}
