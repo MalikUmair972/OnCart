6328d60d1897de79fab46bf2351411aa9c3a0381
MalikUmair972 <mu501362@gmail.com>
2025-04-09 19:32:16 +0500

    Footer nav hide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'oncart';
+  showNav = true;
+  showFooter = true;
+
+  constructor (private router:Router) {}
+  ngOnInit(): void {
+
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(()=> {
+      const currentRoute = this.router.url;
+
+      const isAuthPage = currentRoute.includes('login') || currentRoute.includes('signup')
+
+      this.showNav = !isAuthPage
+      this.showFooter = !isAuthPage
+    })
+  }
 }
