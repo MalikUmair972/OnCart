b06566ef13b9b9af578f83c5033bf8778dbb25ad
MalikUmair972 <mu501362@gmail.com>
2025-04-11 23:50:56 +0500

    Product details module update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { CategoriesComponent } from './pages/categories/categories.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ProductComponent } from './pages/product/product.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'categories', component: CategoriesComponent },
   { path: 'product', component: ProductComponent },
+  { path: 'product-details', component: ProductDetailsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
